52848d4aca4a6cbb2d1600d3a3b0e995dda1eb9b
Naillton <nailton_junior@protonmail.com>
2024-03-24 15:43:43 -0300

    update

--- banner.py
+++ banner.py
@@ -1,7 +1,10 @@
-import socket
-host = input('Digite o ip do alvo: ')
-port = int(input('Digite a porta do dominio: '))
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host,port))
-print (sock.recv(2048))
-sock.close()
+import socket as socket
+try:
+  host = input('Digite o ip do alvo: ')
+  port = int(input('Digite a porta do dominio: '))
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.connect((host,port))
+  print (sock.recv(2048))
+  sock.close()
+except:
+  print ('Ocorreu um erro')
